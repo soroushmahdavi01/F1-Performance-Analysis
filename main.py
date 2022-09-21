c509648482e138f8ac3225c6dcccd9e2374519fc
Soroush Mahdavi <soroush.mahdavi01@gmail.com>
2022-09-20 21:49:25 -0700

    Inital Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# I want to create a program that allows peopel to see performance average increase between teams thorught the years
+# Example Force India Racing point 2019/2020 increase dramticly between the two, count the first race and the last race or the average pace increases. 
+# so 2019 their average pace was 1:19 for thes first race and 1:17 in the next years race for  opening so a 25% increase in pace, now we take that over 25 races and average the overall pace we would get 20 % increase,
+#https://documenter.getpostman.com/view/11586746/SztEa7bL#46c7fbee-e90f-409f-b2ff-d8b77e85e5f6
+#https://ergast.com/mrd/
+
+
+import requests
+
+url = "http://ergast.com/api/f1/2021/21/qualifying"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
